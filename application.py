49c83fa74f6db31ee4178a12bbe55719223f90e7
AbjabjaSalahEddine <76133948+AbjabjaSalahEddine@users.noreply.github.com>
2021-03-09 17:45:17 +0100

    Add files via upload

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 import numpy as np
 import sklearn
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
@@ -23,8 +25,18 @@ test=tk.StringVar()
 def initialisation():
     file_path=tk.StringVar()
     C={}
+    for widget in frame.winfo_children():
+        if isinstance(widget, tk.Label):
+            widget.destroy()
     for widget in frame2.winfo_children():
         widget.destroy()
+    for widget in frame21.winfo_children():
+        widget.destroy()
+    for widget in frame22.winfo_children():
+        widget.destroy()
+    openFile = tk.Button(frame2, text="Importer le fichier contenant le spectre", padx=25,
+                     pady=10, fg='white', bg="#15a328", command=readfile)
+    openFile.pack(side="top", expand="yes")
 
 
 def readmodels():
@@ -34,18 +46,20 @@ def readmodels():
 
 
 def readfile():
+    
     filename = filedialog.askopenfilename(
         initialdir="/", title="Select file", filetypes=(("excel csv", "*.csv"), ("all files", "*.*")))
     file_path.set(filename)
     
     if file_path.get()!="":
-        
+        for widget in frame2.winfo_children():
+            widget.destroy()
         buttonvisible()
 
     
 
 def buttonvisible():
-    label = tk.Label(frame2, text="le path du fichier : "+file_path.get() , bg="white" )
+    label = tk.Label(frame2, text="le path du fichier : "+file_path.get()+"", bg="#b3ffd6" )
     label.pack()
     runApps = tk.Button(frame2, text="Lancer la prediction !",
                     pady=10, fg='white', bg="#15a328" , command=predict )
@@ -53,18 +67,24 @@ def buttonvisible():
     
 
 def predict():
+    for widget in frame2.winfo_children():
+        if isinstance(widget, tk.Button):
+            widget.destroy()
     pred=predictor.prediction(file_path.get(),modelsreader.make_model())
     C["P"]=pred[0][0]
     C["Mg"]=pred[0][1]
     C["N"]=pred[0][2]
     C["K"]=pred[0][3]
     C["Cu"]=pred[0][4]
-    s = "Resultats de la prediction : \n"
+    s = "Resultats de la prediction :\n \n"
     for V in C.keys() :
     	s+="["+V+"] = "+str(C[V])+"\n"
-    label = tk.Label(frame2, text=s , bg="white" )
+    label = tk.Label(frame21, text=s , bg="#b3ffd6" , font=("", 12) )
     label.pack()
-    
+    fig= plt.Figure(figsize=(5, 4), dpi=100)
+    fig.add_subplot(111).plot(predictor.spectre(file_path.get())[1],predictor.spectre(file_path.get())[0])
+    graph = FigureCanvasTkAgg(fig, frame22 ) 
+    graph.get_tk_widget().pack()
 
 
 
@@ -72,7 +92,7 @@ canvas = tk.Canvas(root, height=500, width=700, bg="#15a328")
 canvas.pack()
 
 frame = tk.Frame(root, bg="#15a328" )
-frame.place(relx=0.1, rely=0.05, relwidth=0.8, relheight=0.8)
+frame.place(relx=0.1, rely=0.01, relwidth=0.8, relheight=0.8)
 
 prepare = tk.Button(frame, text="preparer l'envirenement", padx=25,
                      pady=10, fg='black', bg="#b3ffd6", command=readmodels )
@@ -80,24 +100,31 @@ prepare.pack()
 
 
 frame1 = tk.Frame(root, bg="#b3ffd6" )
-frame1.place(relx=0.1, rely=0.19, relwidth=0.8, relheight=0.2)
+frame1.place(relx=0.1, rely=0.145, relwidth=0.8, relheight=0.08)
 
 cons="consignes :\n le fichier importé doit etre sous le format .csv obigatoirement.\n"
-label = tk.Label(frame1, text=cons,bg="#b3ffd6")
+label = tk.Label(frame1, text=cons,bg="#b3ffd6" ,font=("", 9) ,fg="red" )
 label.pack()
 
 frame2 = tk.Frame(root, bg="#b3ffd6")
-frame2.place(relx=0.1, rely=0.4, relwidth=0.8, relheight=0.4)
+frame2.place(relx=0.1, rely=0.239, relwidth=0.8, relheight=0.14)
+frame21 = tk.Frame(root, bg="#b3ffd6")
+frame21.place(relx=0.06, rely=0.39, relwidth=0.27, relheight=0.46)
+frame22 = tk.Frame(root, bg="#b3ffd6")
+frame22.place(relx=0.33, rely=0.39, relwidth=0.64, relheight=0.46)
 
-openFile = tk.Button(frame1, text="Importer le fichier contenant le spectre", padx=25,
+openFile = tk.Button(frame2, text="Importer le fichier contenant le spectre", padx=25,
                      pady=10, fg='white', bg="#15a328", command=readfile)
-openFile.pack()
+openFile.pack(side="top", expand="yes")
+
+
 
 frame3 = tk.Frame(root, bg="#b3ffd6" )
-frame3.place(relx=0.1, rely=0.81, relwidth=0.8, relheight=0.18)
+frame3.place(relx=0.1, rely=0.86, relwidth=0.8, relheight=0.13)
 init = tk.Button(frame3, text="REINITIALISER", padx=25,
                      pady=10, fg='white', bg="#15a328", command=initialisation )
 init.pack(side="top", expand="yes")
+
 
 
 root.mainloop()
